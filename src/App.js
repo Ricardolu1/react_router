cdc5210956e0f0cf06e208287b04d65907af791b
Ricardo <568082704@qq.com>
2019-10-14 17:48:37 +0800

    引入router例子成功

--- src/App.js
+++ src/App.js
@@ -1,75 +1,130 @@
 import React from "react"
 import "./App.css"
 import {useState} from 'react'
-import { Router, Route, Link } from 'react-router'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-  let hash = window.location.hash
-  let initUi=''
-
-  if (hash==='#signup') {
-    initUi='注册'
-  }else if(hash==='#login'){
-    initUi='登录'
-  }else{
-    initUi='欢迎'
-  }
-
-  let [ui,setUi]=useState(initUi)
-
-  let onClickLogin = ()=>{
-    setUi('登录')
-    window.location.hash='login'
-  }
-  let onClickSignUp=()=>{
-    setUi('注册')
-    window.location.hash='signup'
-  }
-  let onClickWelcome=()=>{
-    setUi('欢迎')
-    window.location.hash='welcome'
-  }
-
-  let showUi = ()=>{
-    if (ui==='登录') {
-      return <Box1 />
-    }else if (ui==='注册') {
-      return <Box2 />
-    }else if (ui === '欢迎')  {
-      return <Welcome />
-    }
-  }
-
   return (
-    <div className="App">
-      <button onClick={onClickLogin}>登录</button>
-      <button onClick={onClickSignUp}>注册</button>
-      <button onClick={onClickWelcome}>欢迎</button>
+    <Router>
       <div>
-        {showUi()}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    </div>
-  )
+    </Router>
+  );
 }
 
-function Box1() {
-  return(
-    <div className="box">登录</div>
-  )
-}
-function Box2() {
-  return(
-    <div className="box">注册 </div>
-  )
-}
-function Welcome() {
-  return(
-    <div className="box">欢迎</div>
-  )
+function Home() {
+  return <h2>Home</h2>;
 }
 
+function About() {
+  return <h2>About</h2>;
+}
 
-
-
+function Users() {
+  return <h2>Users</h2>;
+}
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       App
+//     </div>
+//   )
+// }
+
+// function Box1() {
+//   return(
+//     <div className="box">登录</div>
+//   )
+// }
+// function Box2() {
+//   return(
+//     <div className="box">注册 </div>
+//   )
+// }
+// function Welcome() {
+//   return(
+//     <div className="box">欢迎</div>
+//   )
+// }
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -3,19 +3,64 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Router, Route, Link } from 'react-router'
-
-React.render((
-  <Router>
-    <Route path="/" component={App}>
-    </Route>
-  </Router>
-), document.body)
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
-// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+ReactDOM.render(
+  <App />
+, document.querySelector('#root'))
+
+
+
 serviceWorker.unregister();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
